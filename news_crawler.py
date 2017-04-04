ad2c8c34d42b385b2bd788d91aa4594aa81ab8b1
Warner <rampy@vt.edu>
2017-04-04 18:19:31 -0400

    Initial commit

---
+++ news_crawler.py
@@ -0,0 +1,38 @@
+# run "scrapy runspider news_crawler.py -o output.csv
+
+import scrapy
+
+# NewsArticle class represents the object obtained by the spider
+class NewsArticle(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+
+# Spider that scrapes articles for news sites
+class NewsSpider(scrapy.Spider):
+    name = 'newsspider'
+    start_urls = ['https://www.yahoo.com/news/',
+                  'http://www.nytimes.com',
+                  'http://www.washingtonpost.com',
+                  'https://news.google.com/',
+                  'http://www.huffingtonpost.com/',
+                  'http://www.cnn.com/',
+                  'http://www.foxnews.com/',
+                  'http://www.nbcnews.com/',
+                  'http://www.dailymail.co.uk/ushome/index.html',
+                  'https://www.theguardian.com/us',
+                  'https://www.wsj.com/',
+                  'http://abcnews.go.com/',
+                  'http://www.bbc.com/news',
+                  'https://www.usatoday.com/',
+                  'http://www.latimes.com/']
+
+    def parse(self, response):
+        for url in response.xpath('//a'):
+            news = NewsArticle()
+
+            news['title'] = url.xpath('text()').extract()
+            news['link'] = url.xpath('@href').extract()
+
+            yield news
+
+
