cc5e1c1570800674745f8c14008e74a8e4a1534d
Warner <rampy@vt.edu>
2017-05-19 08:09:56 -0400

    Updated news sites, initial ingestion complete, need to convert to df

--- NewsAPI_call.R
+++ NewsAPI_call.R
@@ -6,25 +6,23 @@ source("D:/Projects/creds.R")
 
 # access News API (https://newsapi.org/) to get headline articles
 
-sources <- c("associated-press","bbc-news","bloomberg","business-insider",
-             "buzzfeed","cnbc","cnn","google-news","independent","reuters",
-             "the-economist", "the-huffington-post","the-new-york-times",
-             "the-wall-street-journal", "the-washington-post","time","usa-today")
+sources <- c("associated-press","al-jazeera-english","bbc-news","bloomberg",
+             "business-insider","breitbart-news","cnbc","cnn","google-news",
+             "independent","reuters","the-economist", "the-huffington-post",
+             "newsweek","the-new-york-times","the-wall-street-journal", 
+             "the-washington-post","time","usa-today")
 
 requestLinks <- paste0("https://newsapi.org/v1/articles?source=", 
                        sources,"&apiKey=", news.API.key) 
 # for sorting: "&sortBy=latest"
+
+# loop to retrieve JSON news data into initialized list
 allData <- vector("list", length(sources))
-for(x in 1:length(sources)){
+for(x in seq_along(sources)){
   news.source <- GET(requestLinks[x])
   news.source <- content(news.source)
   news.source <- fromJSON(toJSON(news.source))
-  allData[[x]] <- news.source
-  allData[[x]] <- data.frame(allData[[x]])
-  allData[[x]] <- allData[[x]] %>%
-                  select(-articles.author)
-  allData[[x]]$articles.title <- allData[[x]]$articles.title %>% unlist
-  #allData[[x]]$articles.description <- allData[[x]]$articles.description %>% unlist
-  allData[[x]]$articles.url <- allData[[x]]$articles.urlToImage %>% unlist}
-  #allData[[x]]$articles.publishedAt <- allData[[x]]$articles.publishedAt %>% unlist}
-                  
+  allData[[x]] <- news.source}
+
+
+# convert list objects to dataframes
